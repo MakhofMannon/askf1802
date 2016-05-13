91554cba8f0c9ecda19b31cc21ba581a83130b11
MakhofMannon <makhofman1379@gmail.com>
2016-05-13 19:50:51 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {170061824},--Sudo users
+    sudo_users = {158586343},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
